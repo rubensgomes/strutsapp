9870b528178ab97ee1f3cb682b4737f3398efaca
Rubens Gomes <rubens.gomes@ninjaone.com>
2024-08-13 03:50:07 -0500

    remove redundant annotation.

--- app/src/main/kotlin/com/rubensgomes/springstruts/StrutsApp.kt
+++ app/src/main/kotlin/com/rubensgomes/springstruts/StrutsApp.kt
@@ -4,9 +4,7 @@ import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.web.servlet.config.annotation.EnableWebMvc
 
-@EnableWebMvc
 @SpringBootApplication
 class StrutsApp
 
